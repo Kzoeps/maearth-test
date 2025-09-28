9f5c4a34a4c3c01a44a8c951693c177221753b3e
kzoeps <kzoepa@gmail.com>
2025-09-28 19:25:30 +0600

    feat init

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ export const METADATA = {
   client_id: `${baseUrl}/client-metadata.json`,
   client_name: "GainForest",
   client_uri: baseUrl,
-  redirect_uris: [`http://127.0.0.1`] as [string, ...string[]],
+  redirect_uris: [`${baseUrl}`] as [string, ...string[]],
   scope: "atproto transition:generic",
   grant_types: ["authorization_code", "refresh_token"] as [
     "authorization_code",
